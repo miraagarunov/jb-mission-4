7bd1949ea18ad8a96e27ce689ec5945bee46614b
Your Name <you@example.com>
2025-04-06 12:06:56 +0300

    finished

--- front/src/app/components/accountOperation/single/single.component.ts
+++ front/src/app/components/accountOperation/single/single.component.ts
@@ -1,18 +1,3 @@
-// import { Component, Input, OnInit } from '@angular/core';
-// import { AccountOperation } from '../../../models/accountOperation/account-operation.model';
-
-// @Component({
-//   selector: 'app-single',
-//   templateUrl: './single.component.html',
-//   styleUrls: ['./single.component.css'],
-// })
-// export class SingleComponent implements OnInit {
-//   @Input() operation: AccountOperation | null = null;
-
-//   constructor() {}
-
-//   ngOnInit(): void {}
-// }
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AccountOperation } from '../../../models/accountOperation/account-operation.model';
--- front/src/app/services/account-operations.service.ts
+++ front/src/app/services/account-operations.service.ts
@@ -1,28 +1,3 @@
-// import { Injectable } from '@angular/core';
-// import { HttpClient } from '@angular/common/http';
-// import { Observable } from 'rxjs';
-// import { AccountOperation } from '../models/accountOperation/account-operation.model';
-
-// @Injectable({
-//   providedIn: 'root',
-// })
-// export class AccountOperationService {
-//   private apiUrl = 'https://your-api-url.com/operations'; // שנה לכתובת ה-API שלך
-
-//   constructor(private http: HttpClient) {}
-
-//   // פונקציה להוספת פעולה חדשה
-//   create(accountOperation: AccountOperation): Observable<AccountOperation> {
-//     return this.http.post<AccountOperation>(this.apiUrl, accountOperation);
-//   }
-
-//   // פונקציה לקבלת כל הפעולות עבור חשבון
-//   getOperations(accountNumber: string): Observable<AccountOperation[]> {
-//     return this.http.get<AccountOperation[]>(
-//       `${this.apiUrl}?accountNumber=${accountNumber}`
-//     );
-//   }
-// }
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
